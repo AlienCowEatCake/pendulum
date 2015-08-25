1d260cb0f90f31804728dae82eb2338aaff9f4de
Peter Zhigalov <peter.zhigalov@gmail.com>
2015-08-25 14:27:38 +0600

    Поправил сборочные скрипты для Qt 4.8.7 и Qt 5.5.0 под Windows

--- buildscripts/win_qt4.8.7_msvc2015_x86.bat
+++ buildscripts/win_qt4.8.7_msvc2015_x86.bat
@@ -2,9 +2,9 @@
 set V_PROJECT=pendulum
 set V_VERS=0.39
 set V_ARCH=x86
-set V_VCVARS="C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
-set V_QTDIR=C:\Qt\5.4.0-msvc2013-static-x86
-set V_BUILDDIR=build_win_qt5.4.0_%V_ARCH%
+set V_VCVARS="C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
+set V_QTDIR=C:\Qt\qt4.8.7-msvc2015-x86-static
+set V_BUILDDIR=build_win_qt4.8.7_msvc2015_%V_ARCH%
 
 call %V_VCVARS% %V_ARCH%
 set PATH=%V_QTDIR%\bin;%PATH%
@@ -16,8 +16,7 @@ mkdir %V_BUILDDIR%
 cd %V_BUILDDIR%
 qmake CONFIG+="release" ..\%V_PROJECT%.pro
 nmake
-copy release\%V_PROJECT%.exe ..\%V_PROJECT%-%V_VERS%_qt5.4.0_%V_ARCH%.exe
+copy release\%V_PROJECT%.exe ..\%V_PROJECT%-%V_VERS%_qt4.8.7_msvc2015_%V_ARCH%.exe
 cd ..
 
 pause
-
