361930446df6ea3fbe550c0b1319ea9f68037721
Peter Zhigalov <peter.zhigalov@gmail.com>
2016-07-30 16:45:00 +0700

    Доделал сборочные скрипты для OS X

--- buildscripts/osx_qt5.6_clang64_libstdc++.sh
+++ buildscripts/osx_qt5.6_clang64_libstdc++.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 PROJECT=pendulum
-BUILDDIR=build_osx_qt5.6_clang64
+BUILDDIR=build_osx_qt5.6_clang64_libstdc++
 APPNAME="Pendulum"
-DMGNAME="${PROJECT}_qt5.6_clang64"
+DMGNAME="${PROJECT}_qt5.6_clang64_libstdc++"
 
-QTDIR="/opt/Qt/5.6/clang_64"
+QTDIR="/opt/Qt/5.6.1/clang_64_libstdc++_sdk10.9"
 CMD_QMAKE="${QTDIR}/bin/qmake"
 CMD_DEPLOY="${QTDIR}/bin/macdeployqt"
 
@@ -12,8 +12,9 @@ cd "$(dirname $0)"/..
 rm -rf "${BUILDDIR}"
 mkdir -p "${BUILDDIR}"
 cd "${BUILDDIR}"
-${CMD_QMAKE} CONFIG+="release" QMAKE_MACOSX_DEPLOYMENT_TARGET=10.7 "../${PROJECT}.pro"
-make
+${CMD_QMAKE} CONFIG+="release" "../${PROJECT}.pro"
+make -j3
+sed -e 's/10.7.0/10.6.0/' -i "" "${APPNAME}.app/Contents/Info.plist"
 RES_PATH="${APPNAME}.app/Contents/Resources"
 rm -f "${RES_PATH}/empty.lproj"
 mkdir -p "${RES_PATH}/en.lproj" "${RES_PATH}/ru.lproj"
